e2cf9cd201e76c9c18d71e44bc68ad0e7f5b5f82
Yaping Lang <lilyyapinglang1024@outlook.com>
2026-01-09 02:03:09 +0800

    fix rl_clearhistory and make file fclean clean err

--- srcs/safe_functions/quitshell.c
+++ srcs/safe_functions/quitshell.c
@@ -1,9 +1,9 @@
-#include "safefunctions.h"
-#include "parse.h"
 #include "minishellparse.h"
+#include "parse.h"
+#include "safefunctions.h"
 
 void	error(const char *context, char *description, int exit_status,
-	t_shell_context *sh)
+		t_shell_context *sh)
 {
 	ft_write_fd("minishell: ", STDERR_FILENO);
 	ft_write_fd(description, STDERR_FILENO);
@@ -19,10 +19,11 @@ void	quit_shell(int exit_status, t_shell_context *sh)
 	{
 		if (sh->in_main_process && isatty(STDIN_FILENO))
 			ft_write_fd("exit\n", STDERR_FILENO);
-		//clear_prompt(sh); did not clear duing testing
-		//ft_lstclear(&sh->env, free_env_var); did not clean the env part during testing
+		// clear_prompt(sh); did not clear duing testing
+		// ft_lstclear(&sh->env,
+		//	free_env_var); did not clean the env part during testing
 		ft_lstclear(&sh->allocated_pointers[SH], free);
 	}
-	rl_clear_history();
+	// rl_clear_history();
 	exit(exit_status);
 }
