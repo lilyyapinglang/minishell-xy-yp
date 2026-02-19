23dd57d5464a90ed8d31e59fb5505d837d9a2c5b
Yaping Lang <ylang@f4r11s11.clusters.42paris.fr>
2026-02-19 20:39:24 +0100

    104->93

--- build/dep/lib/get_next_line/get_next_line_utils.d
+++
@@ -1,5 +0,0 @@
-build/obj/lib/get_next_line/get_next_line_utils.o: \
-  lib/get_next_line/get_next_line_utils.c \
-  lib/get_next_line/get_next_line.h
-
-lib/get_next_line/get_next_line.h:
--- build/dep/lib/libft/ft_isalpha.d
+++
@@ -1,4 +0,0 @@
-build/obj/lib/libft/ft_isalpha.o: lib/libft/ft_isalpha.c \
-  lib/libft/libft.h
-
-lib/libft/libft.h:
